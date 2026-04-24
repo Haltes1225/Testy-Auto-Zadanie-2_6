0020bade989b5c892624189457651814182ffef6
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-04-24 23:11:59 +0200

    5th commit

--- test_store_pom.py
+++ test_store_pom.py
@@ -20,20 +20,27 @@ def assert_amount(driver, search_phrase, amount):
 
     assert len(page.search_results) == amount
 
-def test_search(driver):
+def assert_amount_compare(driver, search_phrase_1, search_phrase_2):
 
-    url = "https://kodilla.com/pl/test/store"
-    driver.get(url)
+    phrase_type = type(search_phrase_1)
+    if not phrase_type is str:
+        raise TypeError(f"amount must be str type, is {phrase_type}")
+
+    phrase_type = type(search_phrase_2)
+    if not phrase_type is str:
+        raise TypeError(f"amount must be str type, is {phrase_type}")
     
     page = KodillaStorePom(driver)
 
     page.search_bar.clear()
-    page.search_bar.send_keys("Laptop")
+    page.search_bar.send_keys(search_phrase_1)
+    search_results_1 = page.search_results
 
-    print("\n")
-    for result in page.search_results:
-        print(result.text)
-        print("\n")
+    page.search_bar.clear()
+    page.search_bar.send_keys(search_phrase_2)
+    search_results_2 = page.search_results
+
+    assert len(search_results_1) == len(search_results_2)
 
 def test_search_result_amount(driver):
     url = "https://kodilla.com/pl/test/store"
@@ -46,4 +53,12 @@ def test_search_result_amount(driver):
     assert_amount(driver, "Gaming", 1)
     assert_amount(driver, "Powerful", 0)
 
+def test_search_result_case_sensitivity(driver):
+    url = "https://kodilla.com/pl/test/store"
+    driver.get(url)
+
+    assert_amount_compare(driver, "Laptop", "laptop")
+    assert_amount_compare(driver, "Laptop", "LAPTOP")
+    assert_amount_compare(driver, "Laptop", "lApToP")
+    assert_amount_compare(driver, "Laptop", "lAPTOP")
 
