9107d8d7f67404907a81020e2efb9995cc6b9f36
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-04-24 22:52:51 +0200

    4th commit

--- pom.py
+++ pom.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 class KodillaStorePom:
 
@@ -15,6 +16,9 @@ class KodillaStorePom:
 
     @property
     def search_results(self):
-        return WebDriverWait(self.driver,10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".element"))
-    )
+        try:
+            return WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".element"))
+            )
+        except TimeoutException:
+            return []
